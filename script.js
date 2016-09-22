46e3534d75da0278d57ed7384cca7755c8c2ebf4
ponchique <anastasiia.norenko@gmail.com>
2016-09-22 17:37:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,20 +16,20 @@ var sb = '<sub>';
 var sbc = '</sub>';
 
 msg = {
-    'fw': d + p + p + p + p + p, //the
-    'tw': d + d + d + d + p,
+    'fw': d +ss +p +p +p +p +ss +p, 
+    'tw': d +d +d +ss +d +p,
     'sw': [
-        [d + p + d + p + p + p + d + p + d + d + p], //cup
-        [d + p + p + p + d + d + d + d + d + d + d + p + d], //book
-        [d + p + p + p + d + d + d + d + p + p + d] //box
+        [d +p +d +p +ss +p +p +d +ss +p +d +d +p], 
+        [d +p +p +p +ss +d +d +d +ss +d +d +d +ss +d +p +d], 
+        [d +p +p +p +ss +d +d +d +ss +d +p +p +d] 
 
-    ], //on
+    ], 
     'lw': [
-        [d + p + d + d + p + p + p + p + d + p + p + p], //table
-        [p + p + p + d + p + d + d + p + d + p + p], //stand
-        [p + d + d + p + p + d + p + d + p + p + d + d + d + p + d + d] //window
+        [d +ss +p +d +ss +d +p +p +p +ss +p +d +p +p +ss +p],
+        [p +p +p +ss +d +ss +p +d +ss +d +p +ss +d +p +p], 
+        [p +d +d +ss +p +p +ss +d +p +ss +d +p +p +ss +d +d +d +ss +p +d +d] 
     ],
-    'k': d + d + d + p + p //8
+    'k': d +d +d +p +p 
 };
 
 stroke.innerHTML = msg.fw + s + b + msg.sw[0] + bc + sb + 0 + sbc + bc + s + msg.tw + s + msg.fw + s + b + msg.lw[0] + bc + sb + 0 + sbc + bc + s + s + msg.k + msg.k;
