52122cd4d4e73a655062c0a145ea7ed6118cf65a
Ponchique <anastasiia.norenko@gmail.com>
2016-11-07 00:21:14 +0200

    undefined

--- undefind/undefind.js
+++ undefind/undefind.js
@@ -6,33 +6,35 @@ var classes = {
 }
 
 for ( var i = 0; i < 40; i++) {
-    debugger;
     var div  = document.createElement('div')
-    var span = document.createElement('span');
+    var spanElem = document.createElement('span');
     function random () {
         return Math.floor(Math.random() * (6));
     }
 
-    span.className = (classes.paddings[random()] + ' hide-color');
-    span.innerHTML = classes.text[random()];
+    spanElem.className = (classes.paddings[random()] + ' hide-color');
+    spanElem.innerHTML = classes.text[random()];
 
     for (var y = 0; y < classes.text.length; y++) {
-    if (span.innerHTML == classes.text[y]) {
-    span.style.color = classes.colors[y];
-      }
+        if (spanElem.innerHTML == classes.text[y]) {
+            spanElem.style.color = classes.colors[y];
+        }
     }
-     main.appendChild(span)
+     main.appendChild(spanElem)
     }
 
 
 function setColor (event) {   
         var current = random();
         var elem = event.target
-        var text = elem.innerHTML
-        elem.classList.splice(1,1)
-        console.log(elem.classList)
-      
-        
+        var text = elem.innerHTML 
+
+        var elements = document.getElementsByTagName('span');
+
+        for (var x = 0; x < elements.length; x++) {
+            if (elements[x].innerHTML == text)
+            elements[x].className = elem.classList[0];
+        }
     }
 
     main.addEventListener('click', setColor);
