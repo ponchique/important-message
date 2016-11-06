4e929e94eb99c1139b9a74defca0e44b8b5e902b
Ponchique <anastasiia.norenko@gmail.com>
2016-11-06 18:42:42 +0200

    undefined

--- undefind/undefind.js
+++ undefind/undefind.js
@@ -1,7 +1,8 @@
 
 var classes = {
     paddings: ['font-1','font-2','font-3','font-4','font-5','font-6'],
-    text: ['My clothes', 'My hair', 'My flowers', "My pets", "My favorite games","My job"]
+    text: ['My clothes', 'My hair', 'My flowers', "My pets", "My favorite games","My job"],
+    colors: ['yellow', 'blue', 'purpule', 'red', 'lightgreeen','pink']
 }
 
 for ( var i = 0; i < 40; i++) {
@@ -10,16 +11,28 @@ for ( var i = 0; i < 40; i++) {
     function random () {
         return Math.floor(Math.random() * (6));
     }
+
     span.className = classes.paddings[random()]
     span.innerHTML = classes.text[random()];
     main.appendChild(span)
-
-    function setColor (event) {
-        console.log(event.target)
     }
 
-    main.addEventListener('click', setColor) 
-     
+
+function setColor (event) {   
+        debugger;     
+        //var current = random();
+        var elem = event.target
+        console.log(elem.className)
+
+        
+        var currentClass = document.querySelectorAll("." + elem.className)
+        console.log(currentClass)
+        for (var i = 0; i < currentClass.length; i++) {
+            currentClass[i].style.color = 
+        }
+      
+        
     }
 
+    main.addEventListener('click', setColor);
 
